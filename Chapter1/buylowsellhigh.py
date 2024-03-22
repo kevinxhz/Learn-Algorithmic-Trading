5e95d66befc4405950595901431753906393c760
kevinxhz <billion.kevin@yahoo.com>
2024-03-21 20:39:10 -0700

    Add notes to readme.md,modify some display style of the contents of this file

--- Chapter1/buylowsellhigh.py
+++ Chapter1/buylowsellhigh.py
@@ -1,12 +1,13 @@
 from pandas_datareader import data
 import yfinance as yf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 yf.pdr_override()
 start_date = '2014-01-01'
 end_date = '2018-01-01'
 goog_data = data.get_data_yahoo('GOOG', start=start_date, end=end_date)
-
-import numpy as np
-import pandas as pd
 
 goog_data_signal = pd.DataFrame(index=goog_data.index)
 goog_data_signal['price'] = goog_data['Adj Close']
@@ -16,7 +17,6 @@ goog_data_signal['signal'][:] = np.where(goog_data_signal['daily_difference'][:]
 
 goog_data_signal['positions'] = goog_data_signal['signal'].diff()
 
-import matplotlib.pyplot as plt
 fig = plt.figure()
 ax1 = fig.add_subplot(111, ylabel='Google price in $')
 goog_data_signal['price'].plot(ax=ax1, color='r', lw=2.)
@@ -29,11 +29,11 @@ ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index,
          goog_data_signal.price[goog_data_signal.positions == -1.0],
          'v', markersize=5, color='k')
 
-#plt.show()
+# plt.show()
 
 
 # Set the initial capital
-initial_capital= float(1000.0)
+initial_capital = float(1000.0)
 
 positions = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
 portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
@@ -50,6 +50,8 @@ plt.show()
 fig = plt.figure()
 ax1 = fig.add_subplot(111, ylabel='Portfolio value in $')
 portfolio['total'].plot(ax=ax1, lw=2.)
-ax1.plot(portfolio.loc[goog_data_signal.positions == 1.0].index,portfolio.total[goog_data_signal.positions == 1.0],'^', markersize=10, color='m')
-ax1.plot(portfolio.loc[goog_data_signal.positions == -1.0].index,portfolio.total[goog_data_signal.positions == -1.0],'v', markersize=10, color='k')
+ax1.plot(portfolio.loc[goog_data_signal.positions == 1.0].index,
+         portfolio.total[goog_data_signal.positions == 1.0], '^', markersize=10, color='m')
+ax1.plot(portfolio.loc[goog_data_signal.positions == -1.0].index,
+         portfolio.total[goog_data_signal.positions == -1.0], 'v', markersize=10, color='k')
 plt.show()
