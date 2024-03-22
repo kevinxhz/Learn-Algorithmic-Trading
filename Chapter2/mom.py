4d64817959cf9f410eeecc16ecea65d71ef8d63a
kevinxhz <billion.kevin@yahoo.com>
2024-03-21 21:38:11 -0700

    Add notes to readme.md,modify some display style of the contents of this file

--- Chapter2/mom.py
+++ Chapter2/mom.py
@@ -1,16 +1,18 @@
-import pandas as pd
-
 from pandas_datareader import data
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
 
+yf.pdr_override()
 start_date = '2014-01-01'
 end_date = '2018-01-01'
 SRC_DATA_FILENAME = 'goog_data.pkl'
 
 try:
-  goog_data2 = pd.read_pickle(SRC_DATA_FILENAME)
+    goog_data2 = pd.read_pickle(SRC_DATA_FILENAME)
 except FileNotFoundError:
-  goog_data2 = data.DataReader('GOOG', 'yahoo', start_date, end_date)
-  goog_data2.to_pickle(SRC_DATA_FILENAME)
+    goog_data2 = data.get_data_yahoo('GOOG', start=start_date, end=end_date)
+    goog_data2.to_pickle(SRC_DATA_FILENAME)
 
 goog_data = goog_data2.tail(620)
 
@@ -25,25 +27,23 @@ The Momentum (MOM) indicator compares the
 
  MOM =  Price - Price of n periods ago
  '''
-time_period = 20 # how far to look back to find reference price to compute momentum
-history = [] # history of observed prices to use in momentum calculation
-mom_values = [] # track momentum values for visualization purposes
+time_period = 20  # how far to look back to find reference price to compute momentum
+history = []  # history of observed prices to use in momentum calculation
+mom_values = []  # track momentum values for visualization purposes
 
 for close_price in close:
-  history.append(close_price)
-  if len(history) > time_period: # history is at most 'time_period' number of observations
-    del (history[0])
+    history.append(close_price)
+    if len(history) > time_period:  # history is at most 'time_period' number of observations
+        del (history[0])
 
-  mom = close_price - history[0]
-  mom_values.append(mom)
+    mom = close_price - history[0]
+    mom_values.append(mom)
 
 goog_data = goog_data.assign(ClosePrice=pd.Series(close, index=goog_data.index))
 goog_data = goog_data.assign(MomentumFromPrice20DaysAgo=pd.Series(mom_values, index=goog_data.index))
 
 close_price = goog_data['ClosePrice']
 mom = goog_data['MomentumFromPrice20DaysAgo']
-
-import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211, ylabel='Google price in $')
